5333de302870ff98f4f985bd3dedb33e8ba465a9
Ankit ladva <93598355+ankitladva11@users.noreply.github.com>
2023-02-19 13:20:16 +0530

    Time: 11 ms (49.96%), Space: 6.5 MB (77.91%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
-    string process(string s){
-        int count=1;
-        char a=s[0];
-        string sol="";
-        for(int i=1;i<s.length();i++){
-            
-            if(a==s[i]){
-                count++;
-            }
-            else{
-                sol=sol+to_string(count)+a;
-                a=s[i];
-                count=1;
-            }
-        }
-    sol=sol+to_string(count)+a;
-return sol;
-        
-        
-    }
     string countAndSay(int n) {
-        if(n==1){
+        if(n==1)
             return "1";
-            
+        string temp=countAndSay(n-1);
+        int i=0;
+        string str="";
+        while(i<temp.size()){
+            char ch=temp[i];
+            int count=0;
+            while(temp[i]==ch){
+                count++;
+                i++;
+            }
+            str+=to_string(count);
+            str+=ch;
         }
-        string sol="11";
-        
-        for(int i=3;i<=n;i++){
-            string b=process(sol);
-            sol=b;
-        }
-        return sol;
+        return str;
     }
 };
