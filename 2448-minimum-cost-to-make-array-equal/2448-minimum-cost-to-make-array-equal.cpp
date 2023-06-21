6c59d6a8a5bc257f9bd3c551c8747bb34fa53df4
Ankit ladva <93598355+ankitladva11@users.noreply.github.com>
2023-06-21 23:46:10 +0530

    Time: 123 ms (40.88%), Space: 44.8 MB (22.63%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
-
-    long long calc(vector<int>&nums, vector<int>& cost, long long median){
-
-        long long ans = 0;
-
-         for(int i=0;i<nums.size();i++)
-           ans += abs(1ll*nums[i]-median)*(1ll*cost[i]);
-
-         return ans;  
-    }
-
-    long long minCost(vector<int>& nums, vector<int>& cost) {
-
-        long long tot = 0;
+    long long c(long long val ,vector<pair<long long,long long>>&vp){
         long long sum = 0;
-        int n = nums.size();
-
-        vector<pair<int,int>> vec;
-
-        for(int i=0;i<nums.size();i++)
-            vec.push_back({nums[i],cost[i]});
-
-        sort(vec.begin(),vec.end());
-
-        for(int i=0;i<n;i++)
-        sum += vec[i].second;
-
-        long long median;
-        int i=0;
-
-        while(tot < (sum+1)/2 && i<n){
-
-             tot += 1ll*vec[i].second;
-             median = vec[i].first;
-             i++;
-        }    
-        
-        
-        return calc(nums,cost,median);
+        for(int i=0;i<vp.size();i++){
+            sum += abs(val-vp[i].first)*vp[i].second;
+        }
+        return sum;
+    }
+    long long minCost(vector<int>& nums, vector<int>& cost) {
+        vector<pair<long long,long long>>vp;
+         long long weight = 0;
+        for(int i=0;i<nums.size();i++){
+            vp.push_back({nums[i],cost[i]});
+            weight += cost[i];
+        }
+       long long l=*min_element(nums.begin(),nums.end());
+       long long r=*max_element(nums.begin(),nums.end());
+       long long ans=l;
+        while(l<r){
+            long long mid=(l+r)/2;
+            long long c1=c(mid,vp);
+            long long c2=c(mid+1,vp);
+            if(c1>c2){
+                l = mid+1;
+                ans = l;
+            }   
+            else{
+                r = mid;
+                ans = r;
+            }
+        }
+        return c(ans,vp);
     }
 };
