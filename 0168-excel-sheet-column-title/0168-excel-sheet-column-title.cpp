6d98db0c5267d80aa551f6bc6f4df3560bd5708b
Ankit ladva <93598355+ankitladva11@users.noreply.github.com>
2023-08-22 23:55:43 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (11.31%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        std::string result {};
+        while(columnNumber>0){
+            columnNumber-=1;
+            result.insert(result.begin(),65+columnNumber%26);
+            columnNumber/=26;
+        }
+        return result;
+    }
+};
